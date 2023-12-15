2749c35e043de2c262497c7d51d1367f69ee24a4
ZendeAditya <adityazende6710@gmail.com>
2023-12-15 13:30:55 +0530

    fix the error in post method

--- src/app/api/allUsers/route.ts
+++ src/app/api/allUsers/route.ts
@@ -5,7 +5,7 @@ export const POST = async (req, res) => {
   const { name, mobile, email, github } = await req.json();
   await connectDB();
   console.log("post method");
-  Todo.create({ name, mobile, email, github });
+  await Todo.create({ name, mobile, email, github });
   return NextResponse.json({ data: "topic created!" });
 };
 
