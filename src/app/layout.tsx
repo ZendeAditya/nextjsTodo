2f4543c3108a8ea127e908837a830fe9f1246492
ZendeAditya <adityazende6710@gmail.com>
2023-12-16 12:25:19 +0530

    fix styling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,8 +17,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <div className='flex items-center justify-center flex-col'>
         <Hero/>
         {children}
+        </div>
         </body>
     </html>
   )
